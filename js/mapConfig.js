a87c0ad42527e4927563fc14a8a79d5d26a53a44
hwahyo-o <ajas03974@gmail.com>
2026-07-07 16:07:04 +0900

    Add Naver map client config

---
+++ js/mapConfig.js
@@ -0,0 +1,7 @@
+export const NAVER_MAP_CLIENT_ID = "";
+
+export const NAVER_MAP_OPTIONS = {
+  useNaverStyleMap: Boolean(NAVER_MAP_CLIENT_ID),
+  defaultCenter: { lat: 37.5665, lng: 126.978 },
+  defaultZoom: 14
+};
